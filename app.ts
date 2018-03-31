fa3ef1919de7c45459b7382df34fa5597cac8645
Robert Lowe <robertmlowe@rmlowe.com>
2018-04-01 01:01:30 +0800

    Compiler Improvements with TypeScript 2.0

--- app.ts
+++ app.ts
@@ -4,3 +4,12 @@ let anything;
 anything = 12;
 
 // myName = 30;
+
+function controlMe(isTrue: boolean, somethingElse: boolean) {
+    let result: number;
+    if (isTrue) {
+        result = 12;
+    }
+    result = 33;
+    return result;
+}
