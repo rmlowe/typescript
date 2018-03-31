cfab203253150fc059a28025a3c6461e5537ba19
Robert Lowe <robertmlowe@rmlowe.com>
2018-03-31 18:55:18 +0800

    Enums; The "Any" Type

--- app.ts
+++ app.ts
@@ -1,1 +1,39 @@
-console.log("It works!");
+// string
+let myName: string = 'Max';
+// myName = 28;
+
+// number
+let myAge: number = 27.5;
+// myAge = 'Max';
+
+// boolean
+let hasHobbies: boolean = false;
+// hasHobbies = 1;
+
+// assign types
+let myRealAge: number;
+myRealAge = 27;
+// myRealAge = '27';
+
+// array
+let hobbies: any[] = ["Cooking", "Sports"];
+hobbies = [100];
+// hobbies = 100;
+
+// tuples
+let address: [string, number] = ["Superstreet", 99];
+
+//enum
+enum Color {
+    Gray, // 0
+    Green = 100, // 100
+    Blue = 2// 2
+}
+let myColor: Color = Color.Blue;
+console.log(myColor);
+
+// any
+let car: any = "BMW";
+console.log(car);
+car = { brand: "BMW", series: 3};
+console.log(car);
