57ffd50a081f2cfc506a2e7fd1667cfc660c4f50
Robert Lowe <robertmlowe@rmlowe.com>
2018-03-31 23:44:22 +0800

    The "never" Type (added with TypeScript 2.0)

--- app.ts
+++ app.ts
@@ -105,3 +105,8 @@ let finalValue = 30;
 if (typeof finalValue == "number") {
     console.log("Final value is a number");
 }
+
+// never
+function neverReturns():never {
+    throw new Error('An error!');
+}
