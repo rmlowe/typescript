a3b90bc987d9dcee481497a442becdce3c9d1ef3
Robert Lowe <robertmlowe@rmlowe.com>
2018-03-31 19:28:10 +0800

    Functions as Types

--- app.ts
+++ app.ts
@@ -55,3 +55,10 @@ function multiply(value1: number, value2: number): number {
 }
 // console.log(multiply(2, 'Max'));
 console.log(multiply(10, 2));
+
+// function types
+let myMultiply: (a: number, b: number) => number;
+// myMultiply = sayHello;
+myMultiply();
+myMultiply = multiply;
+console.log(myMultiply(5, 2));
