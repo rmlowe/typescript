59e0f43ec87bb96f969a88530e7f5e738795107a
Robert Lowe <robertmlowe@rmlowe.com>
2018-05-12 00:24:34 +0800

    Getters & Setters

--- app.ts
+++ app.ts
@@ -34,3 +34,27 @@ class Max extends Person {
 }
 const max = new Max("max");
 console.log(max);
+
+// Getters & Setters
+class Plant {
+    private _species: string = "Default";
+
+    get species() {
+        return this._species;
+    }
+
+    set species(value: string) {
+        if (value.length > 3) {
+            this._species = value;
+        } else {
+            this._species = "Default";
+        }
+    }
+}
+
+let plant = new Plant();
+console.log(plant.species);
+plant.species = "AB";
+console.log(plant.species);
+plant.species = "Green Plant";
+console.log(plant.species);
