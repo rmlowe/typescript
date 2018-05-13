d5c8511ff94a0e72c03532e1173b10caa21a09b8
Robert Lowe <robertmlowe@rmlowe.com>
2018-05-13 23:40:59 +0800

    Private Constructors & Singletons (added in TypeScript 2.0)

--- app.ts
+++ app.ts
@@ -91,3 +91,20 @@ let newProject = new ITProject();
 console.log(newProject);
 newProject.changeName("Super IT Project");
 console.log(newProject);
+
+// private constructors
+class OnlyOne {
+    private static instance: OnlyOne;
+
+    private constructor(public name: string) {}
+
+    static getInstance() {
+        if (!OnlyOne.instance) {
+            OnlyOne.instance = new OnlyOne('The Only One');
+        }
+        return OnlyOne.instance;
+    }
+}
+
+let wrong = new OnlyOne('The Only One');
+let right = OnlyOne.getInstance();
