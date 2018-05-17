ef88fb0004a2add7e8bcdd7f5f85d23e16778dad
Robert Lowe <robertmlowe@rmlowe.com>
2018-05-17 23:52:01 +0800

    Importing & Exporting Modules

--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
-import { PI, calculateCircumference } from './math/circle';
+import * as Circle from "./math/circle";
+import calc from "./math/rectangle";
 
-console.log(PI);
-console.log(calculateCircumference(10));
-// console.log();
+console.log(Circle.PI);
+console.log(Circle.calculateCircumference(10));
+console.log(calc(20, 50));
