394a2e14525617097f790d8198ff6cc66c4543e0
Robert Lowe <robertmlowe@rmlowe.com>
2018-05-17 22:55:02 +0800

    An Introduction to Namespaces

--- app.js
+++ app.js
@@ -1,125 +1,16 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = Object.setPrototypeOf ||
-        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var Person = /** @class */ (function () {
-    function Person(name, username) {
-        this.username = username;
-        this.age = 27;
-        this.name = name;
+var MyMath;
+(function (MyMath) {
+    var PI = 3.14;
+    function calculateCircumference(diameter) {
+        return diameter * PI;
     }
-    Person.prototype.printAge = function () {
-        console.log(this.age);
-        this.setType("Old Guy");
-    };
-    Person.prototype.setType = function (type) {
-        this.type = type;
-        console.log(this.type);
-    };
-    return Person;
-}());
-var person = new Person("Max", "max");
-console.log(person.name, person.username);
-person.printAge();
-// person.setType("Cool guy"); // Won't work with private method
-// Inheritance
-var Max = /** @class */ (function (_super) {
-    __extends(Max, _super);
-    // name = "Max";
-    function Max(username) {
-        var _this = _super.call(this, "Max", username) || this;
-        _this.age = 31;
-        return _this;
+    MyMath.calculateCircumference = calculateCircumference;
+    function calculateRectangle(width, length) {
+        return width * length;
     }
-    return Max;
-}(Person));
-var max = new Max("max");
-console.log(max);
-// Getters & Setters
-var Plant = /** @class */ (function () {
-    function Plant() {
-        this._species = "Default";
-    }
-    Object.defineProperty(Plant.prototype, "species", {
-        get: function () {
-            return this._species;
-        },
-        set: function (value) {
-            if (value.length > 3) {
-                this._species = value;
-            }
-            else {
-                this._species = "Default";
-            }
-        },
-        enumerable: true,
-        configurable: true
-    });
-    return Plant;
-}());
-var plant = new Plant();
-console.log(plant.species);
-plant.species = "AB";
-console.log(plant.species);
-plant.species = "Green Plant";
-console.log(plant.species);
-// Static Properties & Methods
-var Helpers = /** @class */ (function () {
-    function Helpers() {
-    }
-    Helpers.calcCircumference = function (diameter) {
-        return this.PI * diameter;
-    };
-    Helpers.PI = 3.14;
-    return Helpers;
-}());
-console.log(2 * Helpers.PI);
-console.log(Helpers.calcCircumference(8));
-// Abstract Classes
-var Project = /** @class */ (function () {
-    function Project() {
-        this.projectName = "Default";
-        this.budget = 1000;
-    }
-    Project.prototype.calcBudget = function () {
-        return this.budget * 2;
-    };
-    return Project;
-}());
-var ITProject = /** @class */ (function (_super) {
-    __extends(ITProject, _super);
-    function ITProject() {
-        return _super !== null && _super.apply(this, arguments) || this;
-    }
-    ITProject.prototype.changeName = function (name) {
-        this.projectName = name;
-    };
-    return ITProject;
-}(Project));
-var newProject = new ITProject();
-console.log(newProject);
-newProject.changeName("Super IT Project");
-console.log(newProject);
-// private constructors
-var OnlyOne = /** @class */ (function () {
-    function OnlyOne(name) {
-        this.name = name;
-    }
-    OnlyOne.getInstance = function () {
-        if (!OnlyOne.instance) {
-            OnlyOne.instance = new OnlyOne('The Only One');
-        }
-        return OnlyOne.instance;
-    };
-    return OnlyOne;
-}());
-var wrong = new OnlyOne('The Only One');
-var right = OnlyOne.getInstance();
-console.log(right.name);
-right.name = 'Something else';
+    MyMath.calculateRectangle = calculateRectangle;
+})(MyMath || (MyMath = {}));
+var PI = 2.99;
+console.log(MyMath.calculateRectangle(10, 20));
+console.log(MyMath.calculateCircumference(3));
+console.log(PI);
--- app.ts
+++ app.ts
@@ -1,115 +1,17 @@
-class Person {
-    name: string;
-    private type: string;
-    protected age: number = 27;
+namespace MyMath {
+    const PI = 3.14;
 
-    constructor(name: string, public username: string) {
-        this.name = name;
+    export function calculateCircumference(diameter: number) {
+        return diameter * PI;
     }
 
-    printAge() {
-        console.log(this.age);
-        this.setType("Old Guy");
-    }
-
-    private setType(type: string) {
-        this.type = type;
-        console.log(this.type);
+    export function calculateRectangle(width: number, length: number) {
+        return width * length;
     }
 }
 
-const person = new Person("Max", "max");
-console.log(person.name, person.username);
-person.printAge();
-// person.setType("Cool guy"); // Won't work with private method
+const PI = 2.99;
 
-// Inheritance
-class Max extends Person {
-    // name = "Max";
-
-    constructor(username: string) {
-        super("Max", username);
-        this.age = 31;
-    }
-}
-const max = new Max("max");
-console.log(max);
-
-// Getters & Setters
-class Plant {
-    private _species: string = "Default";
-
-    get species() {
-        return this._species;
-    }
-
-    set species(value: string) {
-        if (value.length > 3) {
-            this._species = value;
-        } else {
-            this._species = "Default";
-        }
-    }
-}
-
-let plant = new Plant();
-console.log(plant.species);
-plant.species = "AB";
-console.log(plant.species);
-plant.species = "Green Plant";
-console.log(plant.species);
-
-// Static Properties & Methods
-class Helpers {
-    static PI: number = 3.14;
-    static calcCircumference(diameter: number): number {
-        return this.PI * diameter;
-    }
-}
-console.log(2 * Helpers.PI);
-console.log(Helpers.calcCircumference(8));
-
-// Abstract Classes
-abstract class Project {
-    projectName: string = "Default";
-    budget: number = 1000;
-
-    abstract changeName(name: string): void;
-
-    calcBudget() {
-        return this.budget * 2;
-    }
-}
-
-class ITProject extends Project {
-    changeName(name: string): void {
-        this.projectName = name;
-    }
-}
-
-let newProject = new ITProject();
-console.log(newProject);
-newProject.changeName("Super IT Project");
-console.log(newProject);
-
-// private constructors
-class OnlyOne {
-    private static instance: OnlyOne;
-    public readonly name: string;
-
-    private constructor(name: string) {
-        this.name = name;
-    }
-
-    static getInstance() {
-        if (!OnlyOne.instance) {
-            OnlyOne.instance = new OnlyOne('The Only One');
-        }
-        return OnlyOne.instance;
-    }
-}
-
-let wrong = new OnlyOne('The Only One');
-let right = OnlyOne.getInstance();
-console.log(right.name);
-right.name = 'Something else';
+console.log(MyMath.calculateRectangle(10,20));
+console.log(MyMath.calculateCircumference(3));
+console.log(PI);
