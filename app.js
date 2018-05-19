f5c96eb5fab9a8b3c81a1fc574238c3a1fc29429
Robert Lowe <robertmlowe@rmlowe.com>
2018-05-19 17:06:35 +0800

    Creating a Generic Function

--- app.js
+++ app.js
@@ -5,3 +5,10 @@ function echo(data) {
 console.log(echo("Max"));
 console.log(echo(27));
 console.log(echo({ name: "Max", age: 27 }));
+// Better Generic
+function betterEcho(data) {
+    return data;
+}
+console.log(betterEcho("Max").length);
+console.log(betterEcho("27"));
+console.log(echo({ name: "Max", age: 27 }));
--- app.ts
+++ app.ts
@@ -6,3 +6,12 @@ function echo(data: any) {
 console.log(echo("Max"));
 console.log(echo(27));
 console.log(echo({name: "Max", age: 27}));
+
+// Better Generic
+function betterEcho<T>(data: T) {
+    return data;
+}
+
+console.log(betterEcho("Max").length);
+console.log(betterEcho<number>("27"));
+console.log(echo({name: "Max", age: 27}));
