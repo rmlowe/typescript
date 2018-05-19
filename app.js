8de2bb23354e0b8985657e82cd81e2eec5c9d888
Robert Lowe <robertmlowe@rmlowe.com>
2018-05-20 00:13:36 +0800

    Creating a Generic Class

--- app.js
+++ app.js
@@ -24,3 +24,16 @@ printAll(["Apple", "Banana"]);
 // Generic Types
 var echo2 = betterEcho;
 console.log(echo2("Something"));
+// Generic Class
+var SimpleMath = /** @class */ (function () {
+    function SimpleMath() {
+    }
+    SimpleMath.prototype.calculate = function () {
+        return +this.baseValue * +this.multiplyValue;
+    };
+    return SimpleMath;
+}());
+var simpleMath = new SimpleMath();
+simpleMath.baseValue = "something";
+simpleMath.multiplyValue = 20;
+console.log(simpleMath.calculate());
--- app.ts
+++ app.ts
@@ -31,3 +31,17 @@ printAll<string>(["Apple", "Banana"]);
 const echo2: <T>(data: T) => T = betterEcho;
 
 console.log(echo2<string>("Something"));
+
+// Generic Class
+class SimpleMath<T> {
+    baseValue: T;
+    multiplyValue: T;
+    calculate(): number {
+        return +this.baseValue * +this.multiplyValue;
+    }
+}
+
+const simpleMath = new SimpleMath();
+simpleMath.baseValue = "something";
+simpleMath.multiplyValue = 20;
+console.log(simpleMath.calculate());
