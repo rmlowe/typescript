f7f0a2938f4b6bf6f88008d9f9cf4c5673c5bdc4
Robert Lowe <robertmlowe@rmlowe.com>
2018-05-19 16:59:23 +0800

    Why and What?

--- app.js
+++ app.js
@@ -1,44 +1,7 @@
-function greet(person) {
-    console.log("Hello, " + person.firstName);
+// Simple Generic
+function echo(data) {
+    return data;
 }
-function changeName(person) {
-    person.firstName = "Anna";
-}
-var person = {
-    firstName: "Max",
-    hobbies: ["Cooking", "Sports"],
-    greet: function (lastName) {
-        console.log("Hi, I am " + this.firstName + " " + lastName);
-    }
-};
-// greet({firstName: "Max", age: 27});
-changeName(person);
-greet(person);
-person.greet("Anything");
-var Person = /** @class */ (function () {
-    function Person() {
-    }
-    Person.prototype.greet = function (lastName) {
-        console.log("Hi, I am " + this.firstName + " " + lastName);
-    };
-    ;
-    return Person;
-}());
-var myPerson = new Person();
-myPerson.firstName = "Maximilian";
-myPerson.lastName = "Anything";
-greet(myPerson);
-myPerson.greet("Anything");
-var myDoubleFunction;
-myDoubleFunction = function (value1, value2) {
-    return (value1 + value2) * 2;
-};
-console.log(myDoubleFunction(10, 20));
-var oldPerson = {
-    age: 27,
-    firstName: "Max",
-    greet: function (lastName) {
-        console.log("Hello!");
-    }
-};
-console.log(oldPerson);
+console.log(echo("Max"));
+console.log(echo(27));
+console.log(echo({ name: "Max", age: 27 }));
