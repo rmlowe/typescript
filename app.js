c26754de2fc5790c00befab3c4bc7ffd2d5c1d7d
Robert Lowe <robertmlowe@rmlowe.com>
2018-05-20 18:15:37 +0800

    Creating a Class Decorator

--- app.js
+++ app.js
@@ -1,81 +1,18 @@
-// Simple Generic
-function echo(data) {
-    return data;
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+function logged(constructorFn) {
+    console.log(constructorFn);
 }
-console.log(echo("Max"));
-console.log(echo(27));
-console.log(echo({ name: "Max", age: 27 }));
-// Better Generic
-function betterEcho(data) {
-    return data;
-}
-console.log(betterEcho("Max").length);
-console.log(betterEcho(27));
-console.log(echo({ name: "Max", age: 27 }));
-// Built-in Generics
-var testResults = [1.94, 2.33];
-testResults.push(-2.99);
-console.log(testResults);
-// Arrays
-function printAll(args) {
-    args.forEach(function (element) { return console.log(element); });
-}
-printAll(["Apple", "Banana"]);
-// Generic Types
-var echo2 = betterEcho;
-console.log(echo2("Something"));
-// Generic Class
-var SimpleMath = /** @class */ (function () {
-    function SimpleMath() {
+var Person = /** @class */ (function () {
+    function Person() {
+        console.log("Hi!");
     }
-    SimpleMath.prototype.calculate = function () {
-        return +this.baseValue * +this.multiplyValue;
-    };
-    return SimpleMath;
+    Person = __decorate([
+        logged
+    ], Person);
+    return Person;
 }());
-var simpleMath = new SimpleMath();
-simpleMath.baseValue = "10";
-simpleMath.multiplyValue = 20;
-console.log(simpleMath.calculate());
-// Module Exercise
-var Entry = /** @class */ (function () {
-    function Entry(key, value) {
-        this.key = key;
-        this.value = value;
-    }
-    return Entry;
-}());
-var MyMap = /** @class */ (function () {
-    function MyMap() {
-        this.entries = [];
-    }
-    MyMap.prototype.setItem = function (key, value) {
-        this.entries = this.entries.filter(function (entry) { return entry.key !== key; }).concat({ key: key, value: value });
-    };
-    MyMap.prototype.getItem = function (key) {
-        return this.entries.filter(function (entry) { return entry.key === key; })[0].value;
-    };
-    MyMap.prototype.clear = function () {
-        this.entries = [];
-    };
-    MyMap.prototype.printMap = function () {
-        console.log('{');
-        this.entries.forEach(function (entry) { return console.log('   ', entry.key, ': ', entry.value); });
-        console.log('}');
-    };
-    return MyMap;
-}());
-var numberMap = new MyMap();
-numberMap.setItem('apples', 5);
-numberMap.setItem('bananas', 10);
-numberMap.setItem('apples', 20);
-numberMap.printMap();
-console.log(numberMap.getItem('apples'));
-var stringMap = new MyMap();
-stringMap.setItem('name', "Max");
-stringMap.setItem('age', "27");
-stringMap.setItem('name', "Anna");
-stringMap.printMap();
-console.log(stringMap.getItem('age'));
-stringMap.clear();
-stringMap.printMap();
--- app.ts
+++ app.ts
@@ -1,92 +1,10 @@
-// Simple Generic
-function echo(data: any) {
-    return data;
+function logged(constructorFn: Function) {
+    console.log(constructorFn);
 }
 
-console.log(echo("Max"));
-console.log(echo(27));
-console.log(echo({name: "Max", age: 27}));
-
-// Better Generic
-function betterEcho<T>(data: T) {
-    return data;
-}
-
-console.log(betterEcho("Max").length);
-console.log(betterEcho<number>(27));
-console.log(echo({name: "Max", age: 27}));
-
-// Built-in Generics
-const testResults: Array<number> = [1.94, 2.33];
-testResults.push(-2.99);
-console.log(testResults);
-
-// Arrays
-function printAll<T>(args: T[]) {
-    args.forEach((element) => console.log(element));
-}
-printAll<string>(["Apple", "Banana"]);
-
-// Generic Types
-const echo2: <T>(data: T) => T = betterEcho;
-
-console.log(echo2<string>("Something"));
-
-// Generic Class
-class SimpleMath<T extends number | string, U extends number | string> {
-    baseValue: T;
-    multiplyValue: U;
-    calculate(): number {
-        return +this.baseValue * +this.multiplyValue;
+@logged
+class Person {
+    constructor() {
+        console.log("Hi!");
     }
 }
-
-const simpleMath = new SimpleMath<string, number>();
-simpleMath.baseValue = "10";
-simpleMath.multiplyValue = 20;
-console.log(simpleMath.calculate());
-
-
-
-// Module Exercise
-class Entry<T> {
-    constructor(public readonly key: string, public value: T) {}
-}
-
-class MyMap<T> {
-    private entries: Array<{ readonly key: string, readonly value: T }> = [];
-
-    setItem(key: string, value: T) {
-        this.entries = this.entries.filter(entry => entry.key !== key).concat({ key, value });
-    }
-
-    getItem(key: string) {
-        return this.entries.filter(entry => entry.key === key)[0].value;
-    }
-
-    clear() {
-        this.entries = [];
-    }
-
-    printMap() {
-        console.log('{');
-        this.entries.forEach(entry => console.log('   ', entry.key, ': ', entry.value));
-        console.log('}');
-    }
-}
-
-const numberMap = new MyMap<number>();
-numberMap.setItem('apples', 5);
-numberMap.setItem('bananas', 10);
-numberMap.setItem('apples', 20);
-numberMap.printMap();
-console.log(numberMap.getItem('apples'));
-
-const stringMap = new MyMap<string>();
-stringMap.setItem('name', "Max");
-stringMap.setItem('age', "27");
-stringMap.setItem('name', "Anna");
-stringMap.printMap();
-console.log(stringMap.getItem('age'));
-stringMap.clear();
-stringMap.printMap();
