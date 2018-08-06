45dbdd4d65480e4dd5e729800922850b4364e25a
Robert Lowe <robertmlowe@rmlowe.com>
2018-08-06 14:01:08 +0800

    Method Decorators

--- app.ts
+++ app.ts
@@ -33,3 +33,30 @@ class Plant {
 }
 const plant = new Plant();
 (<any>plant).print();
+
+// Method Decorator
+function editable(value: boolean) {
+    return function(target: any, propName: string, descriptor: PropertyDescriptor) {
+        descriptor.writable = value;
+    }
+}
+
+class Project {
+    projectName: string;
+
+    constructor(name: string) {
+        this.projectName = name;
+    }
+
+    @editable(false)
+    calcBudget() {
+        console.log(1000);
+    }
+}
+
+const project = new Project("Super Project")
+project.calcBudget();
+project.calcBudget = function() {
+    console.log(2000);
+};
+project.calcBudget();
